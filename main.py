453b55046cc7f93267e2035db0b05bd1ff714aaa
Charis Prasoulas <xprasoulas@gmail.com>
2021-01-25 22:32:14 +0200

    try to move paddle

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from game_board import GameBoard
 from paddle import Paddle
+from turtle import Screen
 
-game_board = GameBoard()
+WIDTH = 600
+HEIGHT = 800
+
+screen = Screen()
+screen.title( "Pink Pong" )
+screen.screensize( WIDTH, HEIGHT )
+screen.bgcolor("black")
 paddle = Paddle()
+
+
+def move_up():
+    new_y = paddle.ycor() - 20
+    paddle.goto( (paddle.xcor(), new_y) )
+
+
+screen.listen()
+screen.onkey(move_up, "Up")
+
+screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,17 +1,24 @@
 from turtle import Turtle
 
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self):
+        super().__init__()
         self.width = 20
         self.height = 100
-        self.position = (0, 0)
+        self.position = (360, 0)
         self.create_paddle()
 
     def create_paddle(self):
-        stack = []
-        for _ in range(3):
-            square2 = Turtle("square")
-            square2.penup()
-            square2.color("white")
-            stack.append(square2)
+        square = Turtle("square")
+        square.penup()
+        square.color("white")
+        square.shapesize(stretch_len=1, stretch_wid=3)
+        square.speed("fastest")
+        square.setposition(self.position)
+
+
+    def move_up(self):
+        new_y = self.ycor() - 20
+        self.goto((self.xcor(), new_y))
+
