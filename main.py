7df4ac0e03ed863e5d3153f28957d35022a97b59
charis prasoulas <xprasoulas@gmail.com>
2021-01-26 15:12:07 +0200

    Remove Class working pad movement

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from paddle import Paddle
-from turtle import Screen
+# from paddle import Paddle
+from turtle import Screen, Turtle
 
 WIDTH = 600
 HEIGHT = 800
-
+position = (450, 0)
 screen = Screen()
-screen.title( "Pink Pong" )
-screen.screensize( WIDTH, HEIGHT )
+screen.title("Pink Pong")
+screen.screensize(WIDTH, HEIGHT)
 screen.bgcolor("black")
-paddle = Paddle()
+
+square = Turtle("square")
+square.penup()
+square.color("white")
+square.shapesize(stretch_len=1, stretch_wid=5)
+square.speed("fastest")
+square.setposition(position)
 
 
 def move_up():
-    new_y = paddle.ycor() - 20
-    paddle.goto( (paddle.xcor(), new_y) )
+    new_y = square.ycor() + 20
+    square.goto((square.xcor(), new_y))
+
+
+def move_down():
+    new_y = square.ycor() - 20
+    square.goto((square.xcor(), new_y))
 
 
 screen.listen()
 screen.onkey(move_up, "Up")
+screen.onkey(move_down, "Down")
 
 screen.exitonclick()
