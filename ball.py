d66c27686b453b04e7edd40fdb8c0856f911ce9b
charis prasoulas <xprasoulas@gmail.com>
2021-01-27 13:44:41 +0200

    Score and collision

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
@@ -7,9 +8,29 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.speed(1)
-
+        self.x_move = 10
+        self.y_move = 10
 
     def move_it(self):
-        self.setheading(45)
-        self.forward(500)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+        # Collision with top
+        if self.ycor() > 300 or self.ycor() < -300:
+            self.bounce_y()
+
+
+
+    # Change the angle when collision is made with walls
+    def bounce_y(self):
+        self.y_move *= -1
+
+    # Change when collision is made with paddle
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from turtle import Screen
 from ball import Ball
 from paddle import Paddle
-
+from scoreboard import Scoreboard
 WIDTH = 600
 HEIGHT = 800
-
+scoreboard = Scoreboard()
 screen = Screen()
 screen.title("Pink Pong")
 screen.screensize(WIDTH, HEIGHT)
@@ -13,15 +13,28 @@ screen.tracer()
 r_paddle = Paddle(x_position=(400, 0))
 l_paddle = Paddle(x_position=(-400, 0))
 ball = Ball()
-ball.move_it()
+
 screen.listen()
-screen.onkey(r_paddle.move_up, "Up")
-screen.onkey(r_paddle.move_down, "Down")
-screen.onkey(l_paddle.move_up, "w")
-screen.onkey(l_paddle.move_down, "s")
+screen.onkeypress(r_paddle.move_up, "Up")
+screen.onkeypress(r_paddle.move_down, "Down")
+screen.onkeypress(l_paddle.move_up, "w")
+screen.onkeypress(l_paddle.move_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move_it()
+    # collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_score()
+
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_score()
 
 screen.exitonclick()
